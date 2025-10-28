fd8ee5f310ace0cf906d7015032ff341868b35d9
yjsecurity <yjsecurity@gmail.com>
2025-10-28 14:59:43 +0900

    Update route.ts

--- app/(chat)/api/chat/route.ts
+++ app/(chat)/api/chat/route.ts
@@ -1,7 +1,7 @@
 import { convertToCoreMessages, Message, streamText } from "ai";
 import { z } from "zod";
 
-import { geminiProModel } from "@/ai";
+import { geminiFlashModel } from "@/ai";
 import {
   generateReservationPrice,
   generateSampleFlightSearchResults,
@@ -33,7 +33,7 @@ export async function POST(request: Request) {
   );
 
   const result = await streamText({
-    model: geminiProModel,
+    model: geminiFlashModel,
 system: 'You are a helpful and creative AI assistant.',
     messages: coreMessages,
     onFinish: async ({ responseMessages }) => {
